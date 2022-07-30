ea2ba106c37dd1eac2cc6d8161da4fb22554300e
sueun lee <1015_s@naver.com>
2022-07-30 22:57:32 +0900

    interview : CRUD, send_interview, checked_interview, time_counting

--- interviews/models.py
+++ interviews/models.py
@@ -1,3 +1,21 @@
+from urllib import response
 from django.db import models
 
 # Create your models here.
+
+class Interview(models.Model):
+    title = models.CharField(max_length=50, null=True, blank=True)
+    method = models.IntegerField(null=True, blank=True)
+    body = models.CharField(max_length=500, null=True, blank=True)
+    # filefield
+    url = models.URLField(max_length=500)
+    deadline = models.DateTimeField()
+    is_send = models.IntegerField(default=0, null=True, blank=True)
+    is_expired = models.IntegerField(default=0, null=True, blank=True)
+
+class Apply(models.Model):
+    interview = models.OneToOneField(Interview, on_delete=models.CASCADE)
+    send_date = models.DateTimeField(default="", null=True, blank=True)
+    check_date = models.DateTimeField(default="", null=True, blank=True)
+    response = models.IntegerField(default=0, null=True, blank=True)
+    hold_reason = models.CharField(max_length=150, null=True, blank=True)
