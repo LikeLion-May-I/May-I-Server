b755ac481b18af7d469832c1efffbd551c8a48b9
myeongjunkim <ilikerunning@naver.com>
2022-08-01 21:46:15 +0900

    feat: register url at profiles.urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from profiles.views import get_profile_all, get_profile_one
+from profiles.views import *
 
 urlpatterns = [
     path('get-profile-one/<int:id>', get_profile_one, name='get_profile_one'),
-    path('get-profile-all/<int:category_id>/', get_profile_all, name='get_profile_all')
+    path('get-profile-all/<int:category_id>/', get_profile_all, name='get_profile_all'),
+    path('register/', RegisterView.as_view())
 ]
