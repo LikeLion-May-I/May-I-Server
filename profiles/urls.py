a699ad0517c1de8b50bfd2f7bb43cbae56360fec
myeongjunkim <ilikerunning@naver.com>
2022-08-01 22:18:35 +0900

    feat: user login func

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,6 @@ from profiles.views import *
 urlpatterns = [
     path('get-profile-one/<int:id>', get_profile_one, name='get_profile_one'),
     path('get-profile-all/<int:category_id>/', get_profile_all, name='get_profile_all'),
-    path('register/', RegisterView.as_view())
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view())
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -6,6 +6,13 @@ from django.contrib.auth.models import update_last_login
 from rest_framework import generics
 from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
+from .models import Profile
+
 
 User=get_user_model()
 
@@ -103,9 +110,47 @@ def get_profile_all(request, category_id):
 #     apply_all = Apply.objects.filter(user=user)
     
 
+
+
+#######################################################
+#######################drf#############################
+#######################################################
+
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
 
-    
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
+
+
+class ProfileView(generics.GenericAPIView):
+    serializer_class = ProfileSerializer
+
+    def patch(self, request):
+        profile = Profile.objects.get(user=request.user)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # data = serializer.validated_data
+        # profile.nickname = data['name']
+        # profile.position = data['department']
+        # profile.subjects = data['background']
+        # if request.data['image']:
+        #     profile.image = request.data['image']
+        profile.save()
+        return Response({"result": "ok"},
+                        status=status.HTTP_206_PARTIAL_CONTENT)
+
+    def get(self, request):
+        profile = Profile.objects.get(user=request.user)
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data)
