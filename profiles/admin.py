a597ce18ffe732d2fad1ac88f2e8a4fc80e47a60
myeongjunkim <ilikerunning@naver.com>
2022-08-01 20:48:51 +0900

    feat: admin.py about user-profile custom

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from profiles.models import Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
+
 
 # Register your models here.
 admin.site.register(Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete =False
+    verbose_name_plural = "profile"
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
