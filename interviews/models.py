c0ace2e7d7853faf3fdbedad2bd6ae22a9c12303
sueun lee <1015_s@naver.com>
2022-08-03 20:19:57 +0900

    reply_rate

--- interviews/models.py
+++ interviews/models.py
@@ -8,14 +8,14 @@ class Interview(models.Model):
     method = models.IntegerField(null=True, blank=True)
     body = models.CharField(max_length=500, null=True, blank=True)
     # filefield
-    url = models.URLField(max_length=500)
-    deadline = models.DateTimeField()
+    url = models.URLField(max_length=500, null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     is_send = models.IntegerField(default=0, null=True, blank=True)
     is_expired = models.IntegerField(default=0, null=True, blank=True)
 
 class Apply(models.Model):
-    interview = models.OneToOneField(Interview, on_delete=models.CASCADE)
-    send_date = models.DateTimeField(default="", null=True, blank=True)
-    check_date = models.DateTimeField(default="", null=True, blank=True)
+    interview = models.OneToOneField(Interview, on_delete=models.CASCADE, null=True, blank=True)
+    send_date = models.DateTimeField(auto_now_add=True)
+    check_date = models.DateTimeField(null=True, blank=True)
     response = models.IntegerField(default=0, null=True, blank=True)
     hold_reason = models.CharField(max_length=150, null=True, blank=True)
